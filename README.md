10a41b93a7f7372a439344bdf397b0314fc6b849
Arnaud Le Doeuff <46919995+arnaudledoeuff@users.noreply.github.com>
2021-04-10 14:05:18 +0200

    🎉 First commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# todolist_javascript
-To do list en Javascript
+# ✔️ Todolist in javascript
+*Arnaud Le Doeuff*
+
+Create a simple "To do list".
+
+Task that need to be completed.
+
+![img_tol](https://www.marketing91.com/wp-content/uploads/2020/03/Top-10-apps-for-To-do-List.jpg)
+
+# Objective
+
+* Have simple to do list
+* Learn Javascript
---
+++ index.js
@@ -0,0 +1,45 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("li");
+var i;
+for (i=0 ; i < myNodelist.length; i++){
+	var span = document.createElement("span");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	myNodelist[i].appendChild(span);
+
+}
+var close =documen.getElementByClassName("close");
+var i;
+for (i=0;i < close.length; i++){
+	close[i].onclick = function(){
+		var div = this.parentElement;
+		div.style.display = "none"
+	}
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list =document.querySelector('ul');
+
+list.addEventListener('click', function(ev){
+	if (ev.target.tagName === 'li'){
+		ev.target.classList.toogle('checked');
+	}
+
+
+},false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+	var li =document.createElement("li");
+	var inputValue=document.getElementById("myInput").value;
+	var t=document.createTextNode(inputValue);
+	li.appendChild(t);
+
+	if (inputValue == ''){
+		alert("You must write a task");
+	} else {
+		document.getElementById("TDL").appendChild(li);
+	}
+	document.getElementById("myInput").value="";
+}
