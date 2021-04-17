d512ccbaf10a42b87a3bd00d04f99aa027a4f68a
Arnaud Le Doeuff <46919995+arnaudledoeuff@users.noreply.github.com>
2021-04-17 12:35:45 +0200

    ⚡️ New version

--- index.js
+++ index.js
@@ -1,56 +1,86 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("li");
-var i;
-for (i=0 ; i < myNodelist.length; i++){
-	var span = document.createElement("span");
-	var txt = document.createTextNode("\u00D7");
-	span.className = "close";
-	span.appendChild(txt);
-	myNodelist[i].appendChild(span);
+// Create task function
+const tdl = document.getElementById('TDL');
 
+function createTask(task) {
+  if (task == '') {
+    alert('You must write something!');
+    return;
+  }
+
+  console.log(`Create task ${task}`)
+
+  const listElement = document.createElement('li');
+  listElement.innerHTML = task
+  tdl.appendChild(listElement)
+
+  const span = document.createElement('span');
+  const txt = document.createTextNode('\u00D7');
+  span.className = 'close';
+  span.onclick = function() {
+    console.log('rr')
+    tdl.removeChild(listElement)
+  };
+  listElement.appendChild(span);
+  span.appendChild(txt);
+
+
+  // span.className = 'close';
+  // span.appendChild(txt);
+  // myNodelist.appendChild(span);
 }
 
-// Click on a close button to hide the current list item
-var close =document.getElementsByClassName("close");
-var i;
-for (i=0;i < close.length; i++){
-	close[i].onclick = function(){
-		var div = this.parentElement;
-		div.style.display = "none"
-	}
-}
+//
+const taskAlreadyInserted = [
+  'Learn to learn',
+  'Play piano',
+  'Read a book',
+  'Hit the gym',
+]
+
+taskAlreadyInserted.forEach((element) => createTask(element));
+
+// Click on a close button to : the current list item
+// var close = document.getElementsByClassName('close');
+// var i;
+// for (i = 0; i < close.length; i++) {
+//   close[i].onclick = function() {
+//     var div = this.parentElement;
+//     div.style.display = 'none'
+//   }
+// }
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 
 list.addEventListener('click', function(ev) {
-	if (ev.target.tagName === 'li') {
-		ev.target.classList.toggle('checked');
-	}
-},false);
+  console.log(ev.target)
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-	var li =document.createElement("li");
-	var inputValue=document.getElementById("myInput").value;
-	var t=document.createTextNode(inputValue);
-	li.appendChild(t);
 
-	if (inputValue == ''){
-		alert("You must write a task");
-	} else {
-		document.getElementById("TDL").appendChild(li);
-	}
-	document.getElementById("myInput").value="";
+// Create  new task
 
-	// var span = document.createElement("span");
-	// var txt = document.createTextNode("\u00D7")
-	// span.appendChild(txt);
-	// li.appendChild(span);
-	// for (i=0; i< close.length; i++){
-	// 	close[i].onclick = function(){
-	// 		var div = this.parentElement;
-	// 		div.style.display= "none";
-	// 	}
-	// }
+const myInput = document.getElementById('myInput')
+
+function createTaskFromUserInput() {
+  console.log('createTaskFromUserInput Called')
+  const inputValue = myInput.value;
+  createTask(inputValue)
+  myInput.value = ''
 }
+myInput.onkeydown =
+    function() {
+  if (event.key == 'Enter') {
+    console.log('Enter')
+    createTaskFromUserInput()
+  }
+}
+
+
+const addBtn = document.getElementById('addBtn');
+
+console.log('before')
+addBtn.onclick = createTaskFromUserInput;
+console.log('after')
