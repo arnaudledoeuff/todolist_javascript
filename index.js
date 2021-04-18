1151dce4a03fd93aa501888b2e0d3569da3e6362
Arnaud Le Doeuff <46919995+arnaudledoeuff@users.noreply.github.com>
2021-04-18 15:51:19 +0200

    new update

--- index.js
+++ index.js
@@ -1,86 +1,126 @@
 // Create task function
 const tdl = document.getElementById('TDL');
 
-function createTask(task) {
-  if (task == '') {
-    alert('You must write something!');
-    return;
-  }
-
-  console.log(`Create task ${task}`)
-
-  const listElement = document.createElement('li');
-  listElement.innerHTML = task
-  tdl.appendChild(listElement)
-
-  const span = document.createElement('span');
-  const txt = document.createTextNode('\u00D7');
-  span.className = 'close';
-  span.onclick = function() {
-    console.log('rr')
-    tdl.removeChild(listElement)
-  };
-  listElement.appendChild(span);
-  span.appendChild(txt);
-
-
-  // span.className = 'close';
-  // span.appendChild(txt);
-  // myNodelist.appendChild(span);
+function clearTaskView()
+{
+    tdl.innerHTML = '';
 }
 
-//
+function createTaskView(task)
+{
+
+    const listElement = document.createElement('li');
+    listElement.innerHTML = task.name
+    tdl.appendChild(listElement)
+
+    const span = document.createElement('span');
+    const txt = document.createTextNode('\u00D7');
+    span.className = 'close';
+    span.onclick = function() {
+        task.done = true;
+        updateTdlView(taskList);
+    };
+    listElement.appendChild(span);
+    span.appendChild(txt);
+}
+
+function updateTdlView(taskList)
+{
+    clearTaskView()
+    taskList.tasks.forEach(function(task) {
+        if (task.done === false)
+            createTaskView(task)
+    });
+}
+
+// Object creation taskList
+function TaskList()
+{
+    this.tasks = [];
+    this.append = function(task) {
+        if (typeof task === 'string') {
+            this.tasks.push(new Task(task));
+        } else if (typeof task == 'object') {
+            if (task.name === undefined) {
+                console.error('Task need a name property')
+                return;
+            }
+            if (task.done === undefined) {
+                task.done = false
+            }
+            this.tasks.push(task);
+        } else {
+            console.error('Unknown task type')
+        }
+    }
+}
+
+// Object creation task
+
+function Task(name)
+{
+    this.name = name;
+    this.done = false;
+    // this.hello = function() {
+    //   console.log('hello')
+    // }
+}
+
+// console.log(typeof task1 == 'object')
+
+// taskObject.done = true;
+var taskList = new TaskList();
+
+// console.log(task1);
+// console.log(task3);
+
 const taskAlreadyInserted = [
-  'Learn to learn',
-  'Play piano',
-  'Read a book',
-  'Hit the gym',
+    'Learn to learn',
+    'Play piano',
+    'Read a book',
+    'Hit the gym',
 ]
 
-taskAlreadyInserted.forEach((element) => createTask(element));
+taskAlreadyInserted.forEach((element) => taskList.append(element));
 
-// Click on a close button to : the current list item
-// var close = document.getElementsByClassName('close');
-// var i;
-// for (i = 0; i < close.length; i++) {
-//   close[i].onclick = function() {
-//     var div = this.parentElement;
-//     div.style.display = 'none'
-//   }
-// }
+console.log(taskList);
+const jsonString = JSON.stringify(taskList)
+console.log(JSON.parse(jsonString));
 
+updateTdlView(taskList);
+updateTdlView(taskList);
 // Add a "checked" symbol when clicking on a list item
+
 var list = document.querySelector('ul');
 
 list.addEventListener('click', function(ev) {
-  console.log(ev.target)
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
 }, false);
-
 
 // Create  new task
 
 const myInput = document.getElementById('myInput')
 
-function createTaskFromUserInput() {
-  console.log('createTaskFromUserInput Called')
-  const inputValue = myInput.value;
-  createTask(inputValue)
-  myInput.value = ''
+function createTaskFromUserInput()
+{
+    const inputValue = myInput.value;
+    if (inputValue == '') {
+        alert('You must write something!');
+        return;
+    }
+    taskList.append(inputValue)
+    myInput.value = ''
+    updateTdlView(taskList);
 }
-myInput.onkeydown =
-    function() {
-  if (event.key == 'Enter') {
-    console.log('Enter')
-    createTaskFromUserInput()
-  }
+myInput.onkeydown = function() {
+    if (event.key == 'Enter') {
+        createTaskFromUserInput()
+    }
 }
 
+const addBtn
+    = document.getElementById('addBtn');
 
-const addBtn = document.getElementById('addBtn');
-
-console.log('before')
 addBtn.onclick = createTaskFromUserInput;
-console.log('after')
